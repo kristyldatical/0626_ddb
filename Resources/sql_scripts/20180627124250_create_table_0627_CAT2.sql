7b1dfd845fb4e894fe463f937ec4fc30b420ccd4
kristyldatical <kgomes@datical.com>
2018-06-27 12:43:02 -0500

    Datical DB Deployment Packager   Deployment Pipeline: 0626_MultiCatalog_Test   Deploy / Rollback Scripts Processed (Associated Labels): ddl\create_table_0627_CAT2.sql / none (create_table_0627_CAT2.sql)

---
+++ Resources/sql_scripts/20180627124250_create_table_0627_CAT2.sql
@@ -0,0 +1,24 @@
+USE [SOURCE_0626__CAT_1];
+
+:OUT "20180627124250_create_table_0627_CAT2.log"
+USE [SOURCE_0626__CAT_2]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [Schema_1].[Users_Cat2_S1](
+    [User_Id] [int] NOT NULL,
+    [LastName] [varchar](255) NOT NULL,
+    [FirstName] [varchar](255) NULL,
+    [Address] [varchar](255) NULL,
+    [City] [varchar](255) NULL
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+:OUT stdout
